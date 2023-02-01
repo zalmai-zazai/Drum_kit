373b5609fddf3a704015a443524655d807a5e4e7
Zalmai <shahzalmai98@gmail.com>
2023-02-01 02:19:13 -0800

    new script file created

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+ const crash = new Audio()
+ crash.src ="./sounds/crash.mp3"
+
+ const kick = new Audio()
+ kick.src ="./sounds/kick-bass.mp3"
+
+ const snare = new Audio()
+ snare.src ="./sounds/snare.mp3"
+
+ const tom = new Audio()
+ tom.src ="./sounds/tom-1.mp3"
+
+ const tom2 = new Audio()
+ tom2.src ="./sounds/tom-2.mp3"
+
+ const tom3 = new Audio()
+ tom3.src ="./sounds/tom-3.mp3"
+
+ const tom4 = new Audio()
+ tom4.src ="./sounds/tom-4.mp3"
